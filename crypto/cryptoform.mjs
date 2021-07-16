f15c474f4042a700798a2e09b0f2691af3826220
stefvdw <8038047+stefvdw@users.noreply.github.com>
2021-07-16 15:32:49 +0200

    Add files via upload

--- crypto/cryptoform.mjs
+++ crypto/cryptoform.mjs
@@ -34,19 +34,27 @@ export default class CryptoForm extends HTMLFormElement {
 
         if(event.submitter.value == 'delete') {
             this.reset()
-            return CDB.delete(this.key)
+            try {
+                await CDB.delete(this.key)
+                new Toast(`Note '${this.key}' deleted`)
+            } catch (error) {
+                new Toast(error)
+            }
         } else {
             const name = this.elements.name.value.toLocaleLowerCase()
             const message = this.elements.message.value
             const password = this.elements.password.value
-            
-            if(name !== this.key) {
-                await CDB.delete(this.key)
-                this.key = name
-            }
-    
-            await CDB.set(name, message, password)
-            return 
+            try {
+                if(name !== this.key) {
+                    await CDB.delete(this.key)
+                    this.key = name
+                }
+                
+                await CDB.set(name, message, password)
+                new Toast(`Note '${this.key}' saved`)
+            } catch (error) {
+                new Toast(error)
+            } 
         }
           
        
