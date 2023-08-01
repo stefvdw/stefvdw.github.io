a1a84b5c920d5926f9de293680bbc2fb553a8f75
stefvdw <8038047+stefvdw@users.noreply.github.com>
2023-08-01 10:52:17 +0200

    Add call to base for connectedCallback

--- speedometer/speed-o-meter.mjs
+++ speedometer/speed-o-meter.mjs
@@ -18,6 +18,7 @@ export default class SpeedOMeter extends BaseCanvas {
     }
 
     connectedCallback() {
+        super.connectedCallback()
         this.clear()
         this.drawText('click to start')
         this.arc()
