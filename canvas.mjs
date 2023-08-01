43e2e466e1815ebc24612feb0f7dbf7ef99120e9
stefvdw <8038047+stefvdw@users.noreply.github.com>
2023-08-01 14:53:21 +0200

    Properly set boolean attribute

--- canvas.mjs
+++ canvas.mjs
@@ -10,10 +10,10 @@ export default class BaseCanvas extends HTMLCanvasElement {
     }
 
     get wakelock() { return this.hasAttribute('wakelock')}
-    set wakelock(value) { return !!value ? this.setAttribute('wakelock', null) : this.removeAttribute('wakelock')}
+    set wakelock(value) { return !!value ? this.setAttribute('wakelock', "") : this.removeAttribute('wakelock')}
 
     get fullscreen() { return this.hasAttribute('fullscreen')}
-    set fullscreen(value) { return !!value ? this.setAttribute('fullscreen', null) : this.removeAttribute('fullscreen')}
+    set fullscreen(value) { return !!value ? this.setAttribute('fullscreen', "") : this.removeAttribute('fullscreen')}
 
     connectedCallback() {
         this.addEventListener('fullscreenchange', this.handleFullscreen.bind(this))
